4be8ab39d2f543be127367ec8d6e3c79764ed100
afaqrehman98 <afaqrehman98@gmail.com>
2021-01-19 01:21:19 +0500

    Custom Calendar

--- app/src/main/java/com/example/customcalendar/Extensions.kt
+++ app/src/main/java/com/example/customcalendar/Extensions.kt
@@ -2,16 +2,22 @@ package com.example.customcalendar
 
 import android.content.Context
 import android.view.LayoutInflater
+import android.view.View
 import android.widget.TextView
 import androidx.annotation.ColorRes
 import androidx.core.content.ContextCompat
 import java.time.DayOfWeek
+import java.time.format.DateTimeFormatter
 import java.time.temporal.WeekFields
 import java.util.*
+
+fun View.makeInVisible() {
+    visibility = View.INVISIBLE
+}
 internal val Context.layoutInflater: LayoutInflater
     get() = LayoutInflater.from(this)
 
-
+val monthTitleFormatter = DateTimeFormatter.ofPattern("MMMM")
 internal fun Context.getColorCompat(@ColorRes color: Int) = ContextCompat.getColor(this, color)
 
 internal fun TextView.setTextColorRes(@ColorRes color: Int) = setTextColor(context.getColorCompat(color))
--- app/src/main/java/com/example/customcalendar/ui/main/MainFragment.kt
+++ app/src/main/java/com/example/customcalendar/ui/main/MainFragment.kt
@@ -1,6 +1,6 @@
 package com.example.customcalendar.ui.main
 
-import androidx.lifecycle.ViewModelProvider
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -8,12 +8,13 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.core.view.children
-import com.example.customcalendar.R
+import com.example.customcalendar.*
+import com.example.customcalendar.R.color
 import com.example.customcalendar.databinding.Example1CalendarDayBinding
 import com.example.customcalendar.databinding.MainFragmentBinding
-import com.example.customcalendar.daysOfWeekFromLocale
 import com.example.customcalendar.getColorCompat
 import com.example.customcalendar.setTextColorRes
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import com.kizitonwose.calendarview.model.CalendarDay
 import com.kizitonwose.calendarview.model.DayOwner
 import com.kizitonwose.calendarview.ui.DayBinder
@@ -21,11 +22,16 @@ import com.kizitonwose.calendarview.ui.ViewContainer
 import java.time.LocalDate
 import java.time.YearMonth
 
-class MainFragment : Fragment() {
+class MainFragment : Fragment()  {
 
     private lateinit var binding: MainFragmentBinding
     private val selectedDates = mutableSetOf<LocalDate>()
     private val today = LocalDate.now()
+    private val currentMonth = YearMonth.now()
+    private val startMonth = currentMonth.minusMonths(0)
+    private val endMonth = currentMonth.plusMonths(2)
+    private val endDate = today.plusMonths(2)
+    private val nextMonth = currentMonth.plusMonths(1)
 
     companion object {
         fun newInstance() = MainFragment()
@@ -38,20 +44,24 @@ class MainFragment : Fragment() {
         return inflater.inflate(R.layout.main_fragment, container, false)
     }
 
+    @SuppressLint("ResourceAsColor")
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding = MainFragmentBinding.bind(view)
         val daysOfWeek = daysOfWeekFromLocale()
         binding.legendLayout.root.children.forEachIndexed { index, view ->
             (view as TextView).apply {
-                setTextColorRes(R.color.dark_gray)
+                setTextColorRes(color.dark_gray)
             }
         }
 
+        //Text of Button
+        binding.monthButtonOne.text = currentMonth.month.toString()
+        binding.monthButtonTwo.text = currentMonth.plusMonths(1).month.toString()
+        binding.monthButtonThree.text = currentMonth.plusMonths(2).month.toString()
 
-        val currentMonth = YearMonth.now()
-        val startMonth = currentMonth.minusMonths(10)
-        val endMonth = currentMonth.plusMonths(10)
+
+        //Setting up calendar and setting it's scroll to current month
         binding.exOneCalendar.setup(startMonth, endMonth, daysOfWeek.first())
         binding.exOneCalendar.scrollToMonth(currentMonth)
 
@@ -73,32 +83,46 @@ class MainFragment : Fragment() {
                 }
             }
         }
+
         binding.exOneCalendar.dayBinder = object : DayBinder<DayViewContainer> {
             override fun create(view: View) = DayViewContainer(view)
             override fun bind(container: DayViewContainer, day: CalendarDay) {
                 container.day = day
                 val textView = container.textView
                 textView.text = day.date.dayOfMonth.toString()
-                if (day.owner == DayOwner.THIS_MONTH) {
-                    when {
-                        selectedDates.contains(day.date) -> {
-                            textView.setTextColorRes(R.color.white)
-                            textView.setBackgroundResource(R.drawable.example_1_selected_bg)
-                        }
-                        today == day.date -> {
-                            textView.setTextColorRes(R.color.white)
-                            textView.setBackgroundResource(R.drawable.example_1_today_bg)
-                        }
-                        else -> {
-                            textView.setTextColorRes(R.color.black)
-                            textView.background = null
-                        }
-                    }
-                } else {
-                    textView.setTextColorRes(R.color.gray)
+
+                //To disable all the dates before the current date and end date
+                if (day.date.isBefore(today) || day.date.isAfter(endDate)) {
+                    textView.alpha = 0.6f
                     textView.background = null
+                } else {
+                    if (day.owner == DayOwner.THIS_MONTH) {
+                        when {
+                            selectedDates.contains(day.date) -> {
+                                textView.setTextColorRes(color.black)
+                                textView.setBackgroundResource(R.drawable.example_1_selected_bg)
+                            }
+                            today == day.date -> {
+                                textView.setTextColorRes(color.white)
+                                textView.setBackgroundResource(R.drawable.example_1_today_bg)
+                            }
+                            else -> {
+                                textView.setTextColorRes(color.black)
+                                textView.background = null
+                            }
+                        }
+                    } else {
+                        textView.makeInVisible()
+                    }
                 }
+
             }
+        }
+
+        binding.exOneCalendar.monthScrollListener = { month ->
+            val title = nextMonth.month.toString()
+            binding.monthButtonTwo.text = title
+            binding.monthButtonTwo.setBackgroundColor(R.color.green)
         }
 
 
@@ -107,13 +131,13 @@ class MainFragment : Fragment() {
     override fun onStart() {
         super.onStart()
         requireActivity().window.statusBarColor =
-            requireContext().getColorCompat(R.color.example_1_bg_light)
+            requireContext().getColorCompat(color.example_1_bg_light)
     }
 
     override fun onStop() {
         super.onStop()
         requireActivity().window.statusBarColor =
-            requireContext().getColorCompat(R.color.colorPrimaryDark)
+            requireContext().getColorCompat(color.colorPrimaryDark)
     }
 
 }
